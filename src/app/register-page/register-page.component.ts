587ab7025bf6c6612c4e886784002d1c5e00c208
Muniox <muniox20@gmail.com>
2024-07-07 18:37:26 +0200

    Added register page from old project

---
+++ src/app/register-page/register-page.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class RegisterPageComponent {}
